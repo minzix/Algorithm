12e293a2cc996a8f72999d7095c76c113f4b9981
minzix <126869805+minzix@users.noreply.github.com>
2023-08-03 14:19:53 +0900

    [Bronze V] Title: 시험 성적, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,13 @@
+S = input()
+S = int(S)
+
+if 90 <= S <= 100: 
+    print('A')
+elif 80 <= S <= 89: 
+    print('B')
+elif 70 <= S <= 79: 
+    print('C')
+elif 60 <= S <= 69: 
+    print('D')
+else: 
+    print('F')
