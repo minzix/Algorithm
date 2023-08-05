fd530f871eb2c3ed6a91f382cb050ca95b9cf720
minzix <126869805+minzix@users.noreply.github.com>
2023-08-05 17:11:13 +0900

    [Bronze V] Title: 구구단, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.py
@@ -0,0 +1,13 @@
+N = int(input())
+i = 1
+while i < 10: 
+    print(f"{N} * {i} = {i * N}")
+    i += 1
+    
+# < 파이썬 반복문 설명 >
+# 1. for 요소 in 시퀀스: 
+#    예) fruits = ["사과", "바나나", "딸기"]
+#        for fruit in fruits:
+#        print(fruit) 
+# 2. while 조건문: 
+#    예) while N < 10: 
