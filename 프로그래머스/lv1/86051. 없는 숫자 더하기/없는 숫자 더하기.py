8f8b046874dfb8f6d0824710ee63a8c0e3d59660
minzix <126869805+minzix@users.noreply.github.com>
2023-09-29 15:27:10 +0200

    [level 1] Title: 없는 숫자 더하기, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
@@ -5,6 +5,3 @@ def solution(numbers):
             vacant.append(i)
     answer = sum(vacant)
     return answer
-
-# 0~9까지의 숫자를 배열 numbers 의 원소와 하나씩 비교해 같은 수가 없으면 다른 배열에 저장하기
-# 배열의 원소 전체 합 구하기 -> 배열.sum()
